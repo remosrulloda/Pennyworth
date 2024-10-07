e38f8d0ce6ba943df133a0aa3bc288d97f0d2d74
Remo <remosrulloda@gmail.com>
2024-10-06 22:16:15 -0700

    TrayApp fixes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon, QMenu
 from PySide6.QtGui import QIcon, QAction
-from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Tray Application")
+        self.setWindowTitle("Pennyworth")
         self.setGeometry(100, 100, 300, 200)
-        self.setWindowIcon(QIcon("icon.png"))  # Set your icon here
+        self.setWindowIcon(QIcon("icon.png"))  
 
 class TrayApp(QApplication):
     def __init__(self, args):
@@ -17,7 +16,7 @@ class TrayApp(QApplication):
 
         # Create the system tray icon
         self.tray_icon = QSystemTrayIcon(QIcon("icon.png"), self) 
-        self.tray_icon.setToolTip("Tray Application")
+        self.tray_icon.setToolTip("Pennyworth")
 
         # Create a context menu for the tray icon
         tray_menu = QMenu()
@@ -25,8 +24,11 @@ class TrayApp(QApplication):
         open_action.triggered.connect(self.show_window)
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(self.exit_app)
+        settings_action = QAction("Settings", self)
+        settings_action.triggered.connect(self.exit_app)
 
         tray_menu.addAction(open_action)
+        tray_menu.addAction(settings_action)
         tray_menu.addAction(exit_action)
 
         self.tray_icon.setContextMenu(tray_menu)
@@ -41,7 +43,7 @@ class TrayApp(QApplication):
 
     def on_tray_icon_clicked(self, reason):
         # Handle click events for the tray icon
-        if reason == QSystemTrayIcon.Trigger:  # Left-click
+        if reason == QSystemTrayIcon.Trigger: 
             if not self.main_window.isVisible():
                 self.main_window.show()
             else:
