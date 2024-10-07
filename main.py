c15d00f4cb2f6f99dcfbf3624542564700c29681
Remo <remosrulloda@gmail.com>
2024-10-06 22:13:34 -0700

    Tray Application Test

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-import shutil, os
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QSystemTrayIcon, QMenu
+from PySide6.QtGui import QIcon, QAction
+from PySide6.QtCore import Qt
 
-from pathlib import Path
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Tray Application")
+        self.setGeometry(100, 100, 300, 200)
+        self.setWindowIcon(QIcon("icon.png"))  # Set your icon here
 
-source = Path('/home/remo/Downloads')
-dest = '/home/remo/Sync/Fall 24/ics45j/Lectures'
+class TrayApp(QApplication):
+    def __init__(self, args):
+        super().__init__(args)
+        self.main_window = MainWindow()
 
-inputStr = input('File starts with: ')
+        # Create the system tray icon
+        self.tray_icon = QSystemTrayIcon(QIcon("icon.png"), self) 
+        self.tray_icon.setToolTip("Tray Application")
 
-fileResults = list(source.glob(f'{inputStr}*'))
+        # Create a context menu for the tray icon
+        tray_menu = QMenu()
+        open_action = QAction("Open", self)
+        open_action.triggered.connect(self.show_window)
+        exit_action = QAction("Exit", self)
+        exit_action.triggered.connect(self.exit_app)
 
-for file in fileResults:
-    shutil.move(file, dest)
-    print('Successfully moved', file, 'to', dest)
+        tray_menu.addAction(open_action)
+        tray_menu.addAction(exit_action)
 
-        
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.activated.connect(self.on_tray_icon_clicked)
+        self.tray_icon.show()
 
+    def show_window(self):
+        self.main_window.show()
 
+    def exit_app(self):
+        self.quit()
 
+    def on_tray_icon_clicked(self, reason):
+        # Handle click events for the tray icon
+        if reason == QSystemTrayIcon.Trigger:  # Left-click
+            if not self.main_window.isVisible():
+                self.main_window.show()
+            else:
+                self.main_window.hide()
 
-
+if __name__ == "__main__":
+    app = TrayApp([])
+    sys.exit(app.exec())
--- window.py
+++ window.py
@@ -4,14 +4,19 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication, 
     QMainWindow, 
-    QLabel
+    QLabel,
+    QFileDialog,
+    QPushButton,
+    QSystemTrayIcon
 )
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('Pennyworth')
+        self.setGeometry(100, 100, 300, 200)
 
         widget = QLabel("File Rules")
         font = widget.font()
@@ -21,7 +26,15 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(widget)
 
+        sourceBtn = QPushButton("Select Source Folder")
+        sourceBtn.clicked.connect(self.get_directory)
 
+        self.setCentralWidget(sourceBtn)
+
+    def get_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Source Folder")
+        print("Selected directory:" , directory)
+    
 
 app = QApplication([])
 
